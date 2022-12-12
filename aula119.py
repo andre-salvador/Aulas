15b2e4a6bb26c61e155a87dc89ff58adfe58d4cd
André Salvador <andrersalvador@hotmail.com>
2022-12-12 12:01:42 -0300

    Aulas 119 completas

--- aula119.py
+++ aula119.py
@@ -9,62 +9,109 @@
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
 import os
+import json
 
 
+CAMINHO = 'C:\\Users\\André Salvador\\Desktop\\Aulas\\aula119\\'
+CAMINHO += 'aula119.json'
 def mostrar_lista(lista):
+    if not lista:
+        print()
+        print('Nada a ser mostrado')
+        print()
+        return
+
     print()
     print('TAREFAS')
     for i in lista:
         print(i)
     print()
 
+def desfazer_lista(lista, desfazer):
+    if not lista:
+        print('Nada a desfazer')
+        return
+    
+    desfazer.append(lista[-1])
+    lista.pop()
+    mostrar_lista(lista)
 
-lista = []
+def refazer_lista(lista, desfazer):
+    if not desfazer:
+        print('Não há mais itens para refazer')
+        return
+
+    lista.append(desfazer[-1])
+    desfazer.pop()
+    mostrar_lista(lista)
+
+def adicionar(escolha, lista):
+    if escolha not in lista:
+        lista.append(escolha)
+        mostrar_lista(lista)
+    else:
+        decisao = str(input('Você já adicionou essa terefa, deseja adicionar novamente? [S/N]? ')).lower()
+
+        if decisao == 's':
+            lista.append(escolha)
+            mostrar_lista(lista)
+
+def criar_json(lista, CAMINHO):
+    dados = lista
+    with open(CAMINHO, 'w', encoding='utf-8') as file:
+        dados = json.dump(lista, file, ensure_ascii=False, indent=2,)
+    return dados
+
+def ler(lista, CAMINHO):
+    dados = []
+    try:
+        with open(CAMINHO, 'r', encoding='utf-8') as file:
+            dados = json.load(file)
+            mostrar_lista(lista)
+    
+    except FileNotFoundError:
+        print('O arquivo não existe')
+        criar_json(lista, CAMINHO)
+
+    return dados
+
+lista = ler([], CAMINHO)
 desfazer = []
 
 os.system('cls')
 while True:
-    print('Comandos: Listar, Desfazer, Refazer, Limpar tela')
+    print('Comandos: Listar, Desfazer, Refazer, clear')
     escolha = str(input('digite uma tafera ou comando: ')).lower()
 
-    if escolha == 'listar':
-        if len(lista) != 0:
-            mostrar_lista(lista)
-        else:
-            print('Nada a mostrar')
+    comandos = {
+            'listar': lambda: mostrar_lista(lista),
+            'desfazer': lambda: desfazer_lista(lista, desfazer),
+            'refazer': lambda: refazer_lista(lista, desfazer),
+            'clear': lambda: os.system('cls'),
+            'adicionar': lambda: adicionar(escolha, lista)
+        }
 
-    elif escolha == 'desfazer':
-        if len(lista) != 0:
-            desfazer.append(lista[-1])
-            lista.pop()
-            mostrar_lista(lista)
-        
-        else:
-            print()
-            print('Nada a desfazer')
-            print()
-
-    elif escolha == 'refazer':
-        if len(desfazer) != 0:
-            lista.append(desfazer[-1])
-            desfazer.pop()
-            mostrar_lista(lista)
-        
-        else:
-            print()
-            print('Não há mais itens para refazer')
-            print()
-
-    elif escolha == 'limpar tela':
-        os.system('cls')
+    comando = comandos.get(escolha) if comandos.get(escolha) is not None else comandos['adicionar']
+    comando()
     
-    else:
-        if escolha not in lista:
-            lista.append(escolha)
-            mostrar_lista(lista)
-        else:
-            decisao = str(input('Você já adicionou essa terefa, deseja adicionar novamente? [S/N]? ')).lower()
+    criar_json(lista, CAMINHO)
 
-            if decisao == 's':
-                lista.append(escolha)
-                mostrar_lista(lista)
+#     if escolha == 'listar':
+#         mostrar_lista(lista)
+
+#     elif escolha == 'desfazer':
+#         desfazer_lista(lista, desfazer)
+
+#     elif escolha == 'limpar tela':
+#         os.system('cls')
+    
+#     else:
+#         if escolha not in lista:
+#             lista.append(escolha)
+#             mostrar_lista(lista)
+#         else:
+#             decisao = str(input('Você já adicionou essa terefa, deseja adicionar novamente? [S/N]? ')).lower()
+
+#             if decisao == 's':
+#                 lista.append(escolha)
+#                 mostrar_lista(lista)
