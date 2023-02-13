23a502c652f5960acab642231e2d7da4b6a29da2
André Salvador <andrersalvador@hotmail.com>
2023-02-13 12:27:38 -0300

    Aulas sobre POO

---
+++ aula136.py
@@ -0,0 +1,5 @@
+# Relação entre classes: associação, agregação e composição
+# Composição é uma especialização da agregação.
+# Mas nela, quando o objeto 'pai' for apagado, todas
+# as referências do objetos filhos também são apagadas
+
